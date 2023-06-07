707772f69f847908729e03b3f5bfe7376efadca1
Lyallm1 <81273053+Lyallm1@users.noreply.github.com>
2023-06-07 14:09:33 +0800

    .

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+
 const app = express();
-
 app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
+app.get('/*', (_, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
 app.listen(process.env.PORT || 8080);
